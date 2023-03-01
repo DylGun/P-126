79a7b58b7ec171a41c5282453f99797bcf4b7c51
DylGun <78462130+DylGun@users.noreply.github.com>
2023-02-28 17:06:08 -0800

    Add files via upload

---
+++ screens/camera.js
@@ -0,0 +1,8 @@
+import *as React from "react"
+import * as ImagePicker from "expo-image-picker"
+import * as Permissions from "expo-permissions"
+
+
+export default class PickImage extends React.Component{
+getPermissions
+}
